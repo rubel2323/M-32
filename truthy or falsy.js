7c92c9225c11800afdebc19a824c798e32f1cc51
Rubel <mr.adcadre41@gmail.com>
2026-03-20 15:18:48 +0600

    truthy or falsy added

---
+++ truthy or falsy.js
@@ -0,0 +1,20 @@
+const x = {};
+
+if (!x) {
+  console.log("X is right");
+} else {
+  console.log("X is not correct");
+}
+const y = " ";
+if (!!y) {
+  console.log("Y is  right");
+} else {
+  console.log(" y is no correct");
+}
+
+const z = "";
+if (!!z) {
+  console.log("z is correct");
+} else {
+  console.log("Z is not correct");
+}
